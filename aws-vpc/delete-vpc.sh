c6ac7f37cbc08675bc5b3eb84e60fe3633d016ea
abhigyan-709 <abhigkumar709@gmail.com>
2025-04-01 03:50:18 +0530

    delete the vpc in bash added

---
+++ aws-vpc/delete-vpc.sh
@@ -0,0 +1,9 @@
+# here we will delete the vpc in the same way as we are doing in the AWS CLI
+
+DELETE_VPC=$(
+    awdaws ec2 delete-vpc --vpc-id vpc-010d96e68e89ec4e9
+)
+
+echo "VPC is deleted"
+echo $DELETE_VPC
+
